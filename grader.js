fc334e4ddd9f945fe11a737f1eacc47d0313834c
OpenShift guest <sve@fastmail.fm>
2013-07-10 17:06:43 -0400

    hw3 part3

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require("restler");
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,15 +39,15 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlBuffer) {
+    return cheerio.load(htmlBuffer);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtml = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -54,6 +57,12 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+
+var main = function(htmlBuffer){
+    var checkJson = checkHtml(htmlBuffer, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);    
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +74,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <page>', 'url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if(program.url != null){
+        rest.get(program.url).on('complete', function(result){
+            if(result instanceof Error){
+                console.log("invalide url");
+                process.exit(1);
+            }else{
+                main(result);
+            }
+        })
+    }else{
+        main(fs.readFileSync(program.file));
+    }    
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlFile = checkHtml;
 }
